8c2b7592650514d7a4ce703dbc34498ae5a7662a
Sagar <sagar.jobs@outlook.com>
2023-01-20 17:01:25 +1100

    Add Yup validation to specific fields in the form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import { fetchAPI, submitAPI } from "../stub/stubAPI";
+import * as Yup from 'yup';
 
 const BookingForm = () => {
 
@@ -17,7 +18,13 @@ const BookingForm = () => {
             occasion: '',
             guests: 1
         },
+        validationSchema: Yup.object({
+            date: Yup.date().min(new Date().toISOString().slice(0, 10), 'Date cannot be in the past').required('Required'),
+            guests: Yup.number().min(1, 'Must be greater than 0').max(10, 'Maximum no. of guests that can be booked online is 10')
+                .required('Required')
+        }),
         onSubmit: (values, actions) => {
+            console.log("At submit action", formik.values.time);
             submitAPI(values)
                 .then((response) => {
                     console.log(values, "===========", response);
@@ -27,8 +34,7 @@ const BookingForm = () => {
                         occasion: '',
                         guests: 1
                     })
-                    setShowModal(true);
-                    setDate(new Date().toISOString().slice(0, 10));
+                    setShowModal(true);    
                 })
         }
     })
@@ -63,7 +69,12 @@ const BookingForm = () => {
                                 <input type="date" name="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 
                                         focus:border-primary-600 block w-full p-2.5"
                                     value={date}
-                                    onChange={handleDateChange} />
+                                    onChange={handleDateChange}
+                                    onBlur={formik.handleBlur}
+                                    />
+                                {formik.touched.date && formik.errors.date ? (
+                                    <div>{formik.errors.date}</div>
+                                ) : null}
                             </div>
                             <div>
                                 <label htmlFor="time" className="block mb-2 text-sm font-medium text-gray-900">Choose Time</label>
@@ -93,7 +104,11 @@ const BookingForm = () => {
                                 <input type="number" name="guests" id="guests" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 
                                         focus:border-primary-600 block w-full p-2.5" placeholder="1" min={1} max={10}
                                     value={formik.values.guests}
-                                    onChange={formik.handleChange} />
+                                    onChange={formik.handleChange} 
+                                    onBlur={formik.handleBlur}/>
+                                {formik.touched.guests && formik.errors.guests ? (
+                                    <div>{formik.errors.guests}</div>
+                                ) : null}
                             </div>
                         </div>
                         <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4
@@ -116,7 +131,9 @@ const BookingForm = () => {
                                         Reservation Status
                                     </h3>
                                     <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                                        onClick={() => setShowModal(false)}>
+                                        onClick={() => {
+                                            setShowModal(false);
+                                            setDate(new Date().toISOString().slice(0, 10));}}>
                                         <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                         <span className="sr-only">Close modal</span>
                                     </button>
@@ -124,18 +141,19 @@ const BookingForm = () => {
 
                                 <div className="flex items-baseline justify-between p-4 border-b rounded-t">
                                     <p className="text-base leading-relaxed text-gray-500">
-                                        You reservation is confirmed
+                                        You reservation on {new Date(date).toUTCString().split(" ").slice(0,4).join(" ")} is confirmed
                                     </p>
                                     <button type="button" className="px-5 py-2 sm:mt-6 text-sm font-medium text-center text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4
                                              focus:ring-yellow-300 rounded-xl mr-2 mb-2 data-modal"
-                                        onClick={() => setShowModal(false)}>
+                                        onClick={() => {
+                                            setShowModal(false);
+                                            setDate(new Date().toISOString().slice(0, 10));}}>
                                         Continue
                                     </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    //<p>Your reservation has been confirmed</p>
                 )
             }
 
